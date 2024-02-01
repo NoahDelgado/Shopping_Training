870814a1cb2b7c3875b30476bc6b28e29cda6c82
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-02-01 11:18:41 +0100

    feat : Add items getter to Cart class

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -5,4 +5,17 @@ const UpdateCartException = require('./UpdateCartException.js');
 
 module.exports = class Cart {
 
+    //region private attributes
+    _items;
+    constructor(items = []) {
+        this._items = items;
+    }
+
+    get items() {
+        if (this._items.length === 0) {
+            throw new EmptyCartException();
+        }
+        return this._items;
+    }
+
 }
