63ea949534fb2bdb3934b453a6a84b041839a203
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-02-01 11:13:01 +0100

    feat : Add exception classes to Cart.js

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const EmptyCartException = require('./EmptyCartException.js');
+const UpdateCartException = require('./UpdateCartException.js');
+
 module.exports = class Cart {
 
 }
